3cc7ffb89b946506154f8b1025fe730810a5a2ad
YUNUS EMRE VURGUN <64273622+yunusemrejr@users.noreply.github.com>
2022-04-28 19:24:06 +0300

    Update hexi.c

--- hexi.c
+++ hexi.c
@@ -12,14 +12,14 @@ void hexi(int* array, int n){
 
 int main(int argc, char *argv[]){
 	
-	printf ("\n\n Welcome to decimal to hexi. I convert decimal values to hexidecimal.");
-		printf ("\n\n\n" " I need 5 different decimal values and here they are: 12,13,14,15,16");
+	printf ("\n\n Welcome to int to hexi. I convert integer values to hexidecimal.");
+		printf ("\n\n\n" " I need 5 different integer values and here they are: 12,13,14,15,16");
 				printf ("\n\n\n" " Example: 12,13,14,15 and 16 will be :");
 		int array[]={12,13,14,15,16};
 hexi(array , 5);
 				printf ("\n\n\n" " in hexidecimal." "\n" "\n");
 
-				printf ("\n\n\n" " If you want to calculate with different decimals, change the array in the source code and compile again." "\n" "\n");
+				printf ("\n\n\n" " If you want to calculate with different integers, change the array in the source code and compile again." "\n" "\n");
 								printf ("\n\n\n" " -YUNUS EMRE VURGUN 2022" "\n" "\n");
 
 
